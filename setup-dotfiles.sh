68561cfbe9cecf3c2bd08394f8f29cb9968377cc
cosinequanon <sgoder@deem.com>
2014-05-13 18:56:02 -0700

    [setup] copy files insteaed of symlink

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# symlink the dotfiles in this directory
+# copy the dotfiles in this directory
 # to the home directory
 
-echo "Symlinking files to home directory"
+echo "Copying files to home directory"
 echo
 
 for full_file in $(find $(pwd) -type f -name ".*"); do
     file_name=$(echo $full_file | awk -F "/" '{print $NF}')
-    echo "Symlinking $full_file to ~/$file_name"
+    echo "Copying $full_file to ~/$file_name"
     rm -f ~/$file_name
-    ln -s $full_file ~/$file_name 
+    cp $full_file ~/$file_name 
 done
 
 echo
